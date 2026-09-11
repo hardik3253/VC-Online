318257b502dc0152500ddcdf0e940d45aaf94020
hardik3253 <hardikmprajapati7@gmail.com>
2026-09-11 10:50:11 +0530

    improve reviewer name resolution and update demo user data in reviews loop

--- wp-content/themes/vconline-child/tutor/single/course/reviews-loop.php
+++ wp-content/themes/vconline-child/tutor/single/course/reviews-loop.php
@@ -18,6 +18,7 @@ foreach ( $reviews as $review ) : ?>
 	$profile_photo = ( $user && is_a( $user, 'WP_User' ) ) ? get_user_meta( $user->ID, '_tutor_profile_photo', true ) : '';
 	$has_custom_photo = ( $profile_photo && wp_get_attachment_image_url( $profile_photo ) );
 	$avatar_image_src = $has_custom_photo ? wp_get_attachment_image_url( $profile_photo, 'thumbnail' ) : $default_avatar_url;
+	$reviewer_name = ! empty( $review->display_name ) ? $review->display_name : ( ! empty( $review->comment_author ) ? $review->comment_author : __( 'Student', 'tutor' ) );
 	?>
 	<div class="tutor-review-list-item tutor-card-list-item tutor-p-24 tutor-p-lg-40">
 		<div class="tutor-row">
@@ -25,14 +26,14 @@ foreach ( $reviews as $review ) : ?>
 				<div class="tutor-mb-12">
 					<div class="tutor-avatar tutor-avatar-md tutor-review-avatar">
 						<div class="tutor-ratio tutor-ratio-1x1">
-							<img src="<?php echo esc_url( $avatar_image_src ); ?>" alt="<?php echo esc_attr( $review->display_name ); ?>" class="tutor-avatar-img" />
+							<img src="<?php echo esc_url( $avatar_image_src ); ?>" alt="<?php echo esc_attr( $reviewer_name ); ?>" class="tutor-avatar-img" />
 						</div>
 					</div>
 				</div>
 
 				<div class="tutor-reviewer-name tutor-fs-6 tutor-mb-4">
 					<a href="<?php echo esc_url( $profile_url ); ?>" class="tutor-color-black">
-						<?php echo esc_html( $review->display_name ); ?>
+						<?php echo esc_html( $reviewer_name ); ?>
 					</a>
 				</div>
 
